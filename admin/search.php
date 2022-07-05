dd4bd6fc49c48178761817a8cfba636c78df6c2c
Ntinos <konstantinides.kon@gmail.com>
2022-07-05 18:00:57 +0300

    Add files via upload

---
+++ admin/search.php
@@ -0,0 +1,92 @@
+<?php
+	require_once "..\adminCheck.php";
+
+	require_once "accounts/loginDB.php";
+	
+	// Close connection
+    $conn->close();
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+	<header>
+		<meta charset="utf-8">
+		<title>Search Form</title>
+		<style>
+			@import url('https://fonts.googleapis.com/css2?family=KoHo:wght@500&display=swap');
+			body {
+				font-family: 'KoHo', sans-serif;
+				font-size: 18px;
+				text-align: center;
+				margin: 10px auto;
+				width: 500px;
+			}
+			#searchbt{
+				font-family: sans-serif;
+				font-size: 20px;
+				font-weight: bold;
+				position: absolute;
+				transform: translate(-50%, -50%);
+				width: 170px;
+				height: 40px;
+				margin-top: 20px;
+				letter-spacing: 1px;
+				border: 3px solid #8C82FC;
+				background: #fff;
+				color: #8C82FC;
+				border-radius: 40px;
+				cursor: pointer;
+				overflow: hidden;
+				transition: all .35s;
+			}
+			#searchbt:hover{
+				background: skyblue;
+				color: #fff;
+			}
+			.brd{
+				border: 1px solid black;
+				border-radius: 15px;
+				padding: 10px 0;
+				margin: 20px 0;
+			}
+			.none1{
+				display: none;
+			}
+			span{
+				font-size: 14px;
+				color: red;
+			}
+		</style>	
+	</header>
+	<body>
+		<form action="searchDB.php" method="post">
+			
+			<div class="brd">
+				Search Fisrt Name: <input type="text" placeholder="First name" name="search1"><br>
+				<span>*You can leave it blank</span>
+			</div>
+			AND
+			<div class="brd">
+				Search Grind: <select name="search2">
+				<option class="none1" value="" selected>--Select If Users Grind--</option>
+				<option value="Yes">Yes</option>
+				<option value="No">No</option>
+				<option value="">Empty</option>
+				</select><br>
+				<span>*Choose "Empty" to leave it blank</span>
+			</div>
+			AND
+			<div class="brd">	
+				Search Drink: <select name="search3">
+				<option class="none1" value="" selected>--Select How Users Drink--</option>
+				<option value="Sugar">Sugar</option>
+				<option value="Cream">Cream</option>
+				<option value="">Empty</option>
+				</select><br>
+				<span>*Choose "Empty" to leave it blank</span>
+			</div>
+			
+			<input type = "submit" value="Search" id="searchbt">
+		</form>
+	</body>
+</html>
---
+++ admin/welcomeAdmin.php
@@ -0,0 +1,39 @@
+<?php
+	require_once "..\adminCheck.php";
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8">
+		<title>Dashboard - Administrator area</title>
+		<link rel="stylesheet" href="css/style.css"/>
+	</head>
+	<body>
+		<div id="contact">
+			<img id="img" src="images/admin.jpg" alt="a contact us photo" width="90%" height="300px;">
+			<h1>Administrator</h1>
+		</div>
+		<div id="hello">
+			<h1 class="my-5">Hello, <b><?php echo htmlspecialchars($_SESSION["username"]); ?></b>! Welcome back.</h1>
+		</div>
+		<br>
+		
+		<h3 style="margin-left: 10px;">Here are your options:</h3>
+			
+		<div id="buttons">
+			<input type="button" onclick="location='createDB.php'" class="anchorButtons" value="Create a Database"/>
+			<input type="button" onclick="location='showDB.php'" class="anchorButtons" value="Display Database Records"/>
+			<input type="button" onclick="location='search.php'" class="anchorButtons" value="Search Database Records"/>
+		</div>
+		
+		<p id="signbut">
+			<a href="accounts\logout.php" id="signout1">Sign Out</a>
+		</p>
+		
+		<footer>
+			<hr>
+			<h3 id="Cp">Copyright 2021 | <a href="https://github.com/K-Konstantinidis" target="_blank">Site By: Konstantinidis</a></h3>
+		</footer>
+	</body>
+</html>
